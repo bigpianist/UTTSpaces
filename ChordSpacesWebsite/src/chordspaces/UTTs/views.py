58c3b80a4c280f2bc9d5d9211ea31b99830c9bc7
Ryan Groves <groves.ryan@gmail.com>
2014-01-09 23:57:38 -0500

    Got everything working- now you can input names for your transformations as well. Next step is to add shortcuts!

--- ChordSpacesWebsite/src/chordspaces/UTTs/views.py
+++ ChordSpacesWebsite/src/chordspaces/UTTs/views.py
@@ -10,20 +10,22 @@ from Node2D import Node2DGraph
 
 
 
-def Graph2D(request, mode_1, major_interval_1, minor_interval_1, mode_2, major_interval_2, minor_interval_2):
+def Graph2D(request, mode_1, major_interval_1, minor_interval_1, name_1, mode_2, major_interval_2, minor_interval_2, name_2):
     UTT1 = '(<'
     UTT1 += mode_1 + ','
     UTT1 += major_interval_1 + ','
     UTT1 += minor_interval_1 + '>,'
-    UTT1 += '1,A)'
+    UTT1 += '1,' 
+    UTT1 += name_1 + ')'
     UTT2 = '(<'
     UTT2 += mode_2 + ','
     UTT2 += major_interval_2 + ','
     UTT2 += minor_interval_2 + '>,'
-    UTT2 += '1,B)'
+    UTT2 += '1,' 
+    UTT2 += name_2 + ')'
     startChord = chord.Chord([0,4,7])
-    uttS,uttT,dGraph = createUTTSpaceFromStringsAndStartChord(UTT2, UTT1, startChord)
-    context = {'d_graph': dGraph, 'mode_1': mode_1, 'major_interval_1': major_interval_1, 'minor_interval_1': minor_interval_1, 'mode_2': mode_2, 'major_interval_2': major_interval_2, 'minor_interval_2': minor_interval_2}
+    uttS,uttT,dGraph = createUTTSpaceFromStringsAndStartChord(UTT1, UTT2, startChord)
+    context = {'d_graph': dGraph, 'mode_1': mode_1, 'major_interval_1': major_interval_1, 'minor_interval_1': minor_interval_1, 'name_1': name_1, 'mode_2': mode_2, 'major_interval_2': major_interval_2, 'minor_interval_2': minor_interval_2, 'name_2': name_2, 'isValidGraph' : checkUTTSpace(dGraph)}
     print nodeGraph2DToString(dGraph)
     #for nodeX in dGraph:
     #    for nodeY in nodeX:
@@ -33,39 +35,37 @@ def Graph2D(request, mode_1, major_interval_1, minor_interval_1, mode_2, major_i
 #Since both the front end and back end have access to the same graph, 
 #I just passed the indices of the nodes that I want to search
 #There is a problem with that, which is that sometimes the shortest path points to a closer node
-def GraphPath(request, mode_1, major_interval_1, minor_interval_1, mode_2, major_interval_2, minor_interval_2, x1, y1, x2, y2):
+def GraphPath(request, mode_1, major_interval_1, minor_interval_1, name_1, mode_2, major_interval_2, minor_interval_2, name_2, x1, y1, x2, y2):
     UTT1 = '(<'
     UTT1 += mode_1 + ','
     UTT1 += major_interval_1 + ','
     UTT1 += minor_interval_1 + '>,'
-    UTT1 += '1,A)'
+    UTT1 += '1,' 
+    UTT1 += name_1 + ')'
     UTT2 = '(<'
     UTT2 += mode_2 + ','
     UTT2 += major_interval_2 + ','
     UTT2 += minor_interval_2 + '>,'
-    UTT2 += '1,B)'
+    UTT2 += '1,' 
+    UTT2 += name_2 + ')'
 
     startChord = chord.Chord([0,4,7])
-    uttS,uttT,dGraph = createUTTSpaceFromStringsAndStartChord(UTT2, UTT1, startChord)
+    uttS,uttT,dGraph = createUTTSpaceFromStringsAndStartChord(UTT1, UTT2, startChord)
 
-    print "(1,0) is " + chordToString(dGraph[1][0].chord) + ", with stored indices: (" + str(dGraph[1][0].xInd) + "," + str(dGraph[1][0].yInd) + ")"
-    print "(2,1) is " + chordToString(dGraph[2][1].chord) + ", .chordName='" + dGraph[2][1].chordName + "'"
-    print "(3,2) is " + chordToString(dGraph[3][2].chord) + ", .chordName='" + dGraph[3][2].chordName + "'"
-    print "(4,3) is " + chordToString(dGraph[4][3].chord) + ", .chordName='" + dGraph[4][3].chordName + "'"
-    print "(5,4) is " + chordToString(dGraph[5][4].chord) + ", .chordName='" + dGraph[5][4].chordName + "'"
-    print "(3,5) is " + chordToString(dGraph[3][5].chord) + ", .chordName='" + dGraph[3][5].chordName + "'"
-    print "input (" + x1 + "," + y1 + ") is " + chordToString(dGraph[int(x1)][int(y1)].chord)
-    print "input (" + x2 + "," + y2 + ") is " + chordToString(dGraph[int(x2)][int(y2)].chord)
-    print "finding path from chord '" + dGraph[int(x1)][int(y1)].chordName + "' to '" + dGraph[int(x2)][int(y2)].chordName + "'"
-    print "finding path from chord '" + chordToString(dGraph[int(x1)][int(y1)].chord) + "' to '" + chordToString(dGraph[int(x2)][int(y2)].chord) + "'"
+   
+    #print "finding path from chord '" + dGraph[int(x1)][int(y1)].chordName + "' to '" + dGraph[int(x2)][int(y2)].chordName + "'"
+    #print "finding path from chord '" + chordToString(dGraph[int(x1)][int(y1)].chord) + "' to '" + chordToString(dGraph[int(x2)][int(y2)].chord) + "'"
     shortPath = shortestPath(dGraph, chordCompare, dGraph[int(x1)][int(y1)], dGraph[int(x2)][int(y2)].chord)
-    print [chordToString(i.chord) + ':'  for i in shortPath]
+    print getDistancesAndTransformationsFromPath(dGraph, shortPath)
+    #print [chordToString(i.chord) + ':'  for i in shortPath]
     context = {'shortestPath': shortPath}
-    print nodeGraph2DToString(dGraph)
+    #print nodeGraph2DToString(dGraph)
     #for nodeX in dGraph:
     #    for nodeY in nodeX:
     #        print nodeY.chordName
     return render(request, 'UTTs/path.html', context)#UTT1 + ',' + UTT2
+
+#def addShortCut(request, mode_1, major_interval_1, minor_interval_1, mode_2, major_interval_2, minor_interval_2, x1, y1, x2, y2):
 
 
 def index(request):
